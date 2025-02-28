680b04759e26098b39aa4b427e6cde75cb2b6119
AngelAmoFP <angamosan@alum.us.es>
2025-02-28 22:46:46 +0100

    feat: entity Booking T040

---
+++ src/main/java/acme/entities/booking/Booking.java
@@ -0,0 +1,65 @@
+
+package acme.entities.booking;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidNumber;
+import acme.client.components.validation.ValidString;
+import acme.entities.customer.Customer;
+import acme.entities.flight.Flight;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Booking extends AbstractEntity {
+
+	@Mandatory
+	@Column(unique = true)   //Preguntar a esta gente
+	@ValidString(pattern = "^[A-Z0-9]{6,8}$")
+	@Automapped
+	private String		locatorCode;
+
+	@Mandatory
+	@ValidMoment(past = true)
+	@Automapped
+	private Date		purchaseMoment;
+
+	@Mandatory
+	@Enumerated(EnumType.STRING)
+	@Automapped
+	private TravelClass	travelClass;
+
+	@Mandatory
+	@ValidNumber
+	@Automapped
+	private Double		price;
+
+	@Optional
+	@ValidString(pattern = "^\\d{4}$")
+	@Automapped
+	private String		lastCardNibble;
+
+	@ManyToOne
+	@JoinColumn(name = "customer_id")
+	private Customer	customer;
+
+	@ManyToOne
+	@JoinColumn(name = "flight_id")
+	private Flight		flight;
+
+}
