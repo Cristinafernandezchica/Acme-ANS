df2a4f5ac9a254553da009f650a967314d463b37
MARTA AGUILAR <marta.aguilar.morcillo1@gmail.com>
2025-03-04 01:55:29 +0100

    feat: add TrackingLog entity

---
+++ src/main/java/acme/entities/trackingLogs/TrackingLog.java
@@ -0,0 +1,60 @@
+
+package acme.entities.trackingLogs;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidScore;
+import acme.client.components.validation.ValidString;
+import acme.entities.claims.Claim;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class TrackingLog extends AbstractEntity {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@ManyToOne(optional = false)
+	@JoinColumn(name = "claim_id", nullable = false)
+	private Claim				claim;
+
+	@Mandatory
+	@Automapped
+	@ValidMoment(past = false)
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				lastUpdateMoment;
+
+	@Mandatory
+	@ValidString(max = 50)
+	@Automapped
+	private String				undergoingStep;
+
+	@Mandatory
+	@ValidScore
+	@Automapped
+	private double				resolutionPercentage;
+
+	@Optional
+	@ValidString(max = 255)
+	@Automapped
+	private String				resolution;
+
+
+	private Boolean getClaimAccepted() {
+		return this.claim.getAccepted();
+	}
+
+}
