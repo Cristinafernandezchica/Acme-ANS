e1a2ed3e592268871b1d99074ba93ff55c3c697b
luisemmanuelchavezm <emmanuelchavezzzm@gmail.com>
2025-03-07 17:49:18 +0100

    fix: change relations between maintenanceRecord, task, technicians and change min and max values

--- src/main/java/acme/entities/technicians/Technicians.java
+++ src/main/java/acme/entities/technicians/Technicians.java
@@ -5,9 +5,10 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.validation.Valid;
 
-import acme.client.components.basis.AbstractEntity;
+import acme.client.components.basis.AbstractRole;
 import acme.client.components.mappings.Automapped;
 import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
 import acme.client.components.validation.ValidNumber;
 import acme.client.components.validation.ValidString;
 import lombok.Getter;
@@ -16,22 +17,22 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
-public class Technicians extends AbstractEntity {
+public class Technicians extends AbstractRole {
 
 	private static final long	serialVersionUID	= 1L;
 
 	@Mandatory
-	@ValidString(pattern = "^[A-Z]{2-3}\\d{6}$")
+	@ValidString(pattern = "^[A-Z]{2-3}\\d{6}$", min = 8, max = 9)
 	@Column(unique = true)
 	private String				licenseNumber;
 
 	@Mandatory
-	@ValidString(pattern = "^\\+?\\d{6,15}$")
+	@ValidString(pattern = "^\\+?\\d{6,15}$", min = 6, max = 16)
 	@Automapped
 	private String				phoneNumber;
 
 	@Mandatory
-	@ValidString(max = 50)
+	@ValidString(max = 50, min = 1)
 	@Automapped
 	private String				specialisation;
 
@@ -41,8 +42,12 @@ public class Technicians extends AbstractEntity {
 	private Boolean				annualHealthTest;
 
 	@Mandatory
-	@ValidNumber(min = 0)
+	@ValidNumber(min = 0, max = 120)
 	@Automapped
 	private Integer				yearsOfExperience;
 
+	@Optional
+	@ValidString(max = 255, min = 0)
+	@Automapped
+	private String				certifications;
 }
