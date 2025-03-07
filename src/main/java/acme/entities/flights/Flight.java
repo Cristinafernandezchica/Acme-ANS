38754f2e6587a71a74c963c6ee0a8b767cf2c7c2
CRISTINA FERNANDEZ <correoparacristina100@gmail.com>
2025-03-07 02:13:56 +0100

    feat: Calculate the derived attributes
    Refs #79

--- src/main/java/acme/entities/flights/Flight.java
+++ src/main/java/acme/entities/flights/Flight.java
@@ -1,12 +1,12 @@
 
 package acme.entities.flights;
 
+import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.OneToMany;
+import javax.persistence.Transient;
 
 import acme.client.components.basis.AbstractEntity;
 import acme.client.components.datatypes.Money;
@@ -14,9 +14,10 @@ import acme.client.components.mappings.Automapped;
 import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.Optional;
 import acme.client.components.validation.ValidMoney;
-import acme.client.components.validation.ValidNumber;
 import acme.client.components.validation.ValidString;
+import acme.client.helpers.SpringHelper;
 import acme.entities.legs.Leg;
+import acme.entities.legs.LegRepository;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -47,30 +48,83 @@ public class Flight extends AbstractEntity {
 
 	@Optional
 	@Automapped
+	@ValidString(max = 255)
 	private String				description;
 
-	// Probablemente dejar solo el ManyToOne de Leg
-	@OneToMany(mappedBy = "flight", cascade = CascadeType.ALL)
-	private List<Leg>			legs;
-
-	@Mandatory
-	@ValidNumber(min = 0)
-	private Integer				layovers;
+	// Atributos derivados ---------------------------------
 
 
+	@Transient
 	private Date getScheduledDeparture() {
-		return this.legs.getFirst().getScheduledDeparture();
+		Date res;
+		LegRepository repository;
+		List<Leg> wrapper;
+		List<Leg> orderedWrapper;
+
+		repository = SpringHelper.getBean(LegRepository.class);
+		wrapper = repository.findLegsByFlighId(this.getId());
+		orderedWrapper = wrapper.stream().sorted(Comparator.comparing(Leg::getScheduledDeparture)).toList();
+		res = orderedWrapper.getFirst().getScheduledDeparture();
+
+		return res;
 	}
 
+	@Transient
 	private Date getScheduledArrival() {
-		return this.legs.get(this.legs.size() - 1).getScheduledArrival();
+		Date res;
+		LegRepository repository;
+		List<Leg> wrapper;
+		List<Leg> orderedWrapper;
+
+		repository = SpringHelper.getBean(LegRepository.class);
+		wrapper = repository.findLegsByFlighId(this.getId());
+		orderedWrapper = wrapper.stream().sorted(Comparator.comparing(Leg::getScheduledArrival)).toList();
+		res = orderedWrapper.getLast().getScheduledArrival();
+
+		return res;
 	}
 
+	@Transient
 	private String originCity() {
-		return this.legs.getFirst().getDepartureAirport().getCity();
+		String res;
+		LegRepository repository;
+		List<Leg> wrapper;
+		List<Leg> orderedWrapper;
+
+		repository = SpringHelper.getBean(LegRepository.class);
+		wrapper = repository.findLegsByFlighId(this.getId());
+		orderedWrapper = wrapper.stream().sorted(Comparator.comparing(Leg::getScheduledDeparture)).toList();
+		res = orderedWrapper.getFirst().getDepartureAirport().getCity();
+
+		return res;
 	}
 
+	@Transient
 	private String destinationCity() {
-		return this.legs.getLast().getArrivalAirport().getCity();
+		String res;
+		LegRepository repository;
+		List<Leg> wrapper;
+		List<Leg> orderedWrapper;
+
+		repository = SpringHelper.getBean(LegRepository.class);
+		wrapper = repository.findLegsByFlighId(this.getId());
+		orderedWrapper = wrapper.stream().sorted(Comparator.comparing(Leg::getScheduledArrival)).toList();
+		res = orderedWrapper.getLast().getArrivalAirport().getCity();
+
+		return res;
+	}
+
+	@Transient
+	private Integer layovers() {
+		Integer res;
+		LegRepository repository;
+		List<Leg> wrapper;
+
+		repository = SpringHelper.getBean(LegRepository.class);
+		wrapper = repository.findLegsByFlighId(this.getId());
+		res = wrapper.size();
+
+		return res;
+
 	}
 }
