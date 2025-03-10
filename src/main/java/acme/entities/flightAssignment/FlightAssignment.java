e01617669f7a6f75106511668e7fbfdf8d3965c3
candeegtz <candeguti84@gmail.com>
2025-03-11 00:58:30 +0100

    fix: add missing validators

--- src/main/java/acme/entities/flightAssignment/FlightAssignment.java
+++ src/main/java/acme/entities/flightAssignment/FlightAssignment.java
@@ -6,13 +6,17 @@ import java.util.Date;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
 
 import acme.client.components.basis.AbstractEntity;
+import acme.client.components.mappings.Automapped;
 import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.Optional;
 import acme.client.components.validation.ValidMoment;
 import acme.client.components.validation.ValidString;
-import acme.entities.flightCrewMember.FlightCrewMember;
+import acme.realms.flightCrewMember.FlightCrewMember;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -28,23 +32,29 @@ public class FlightAssignment extends AbstractEntity {
 	// Attributes -------------------------------------------------------------
 
 	@Mandatory
+	@Automapped
 	private FlightCrewsDuty		flightCrewsDuty;
 
 	@Mandatory
 	@ValidMoment(past = true)
+	@Temporal(TemporalType.TIMESTAMP)
 	private Date				lastUpdate;
 
 	@Mandatory
+	@Automapped
 	private CurrentStatus		currentStatus;
 
 	@Optional
 	@ValidString(max = 255)
+	@Automapped
 	private String				remarks;
 
 	// Relations -------------------------------------------------------------
 
+	@Mandatory
 	@ManyToOne(optional = false)
 	@JoinColumn(name = "flight_crew_member_id", nullable = false)
+	@Valid
 	private FlightCrewMember	flightCrewMemberAssigned;
 
 }
