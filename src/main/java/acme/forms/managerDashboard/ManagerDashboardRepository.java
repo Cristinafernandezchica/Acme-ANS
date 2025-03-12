c6fb7c4ed93376146b96ab29822c3834aa986a3a
CRISTINA FERNANDEZ <correoparacristina100@gmail.com>
2025-03-13 00:59:29 +0100

    fix: Change some consultations of Manager dashboard repository

--- src/main/java/acme/forms/managerDashboard/ManagerDashboardRepository.java
+++ src/main/java/acme/forms/managerDashboard/ManagerDashboardRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import acme.client.components.datatypes.Money;
 import acme.client.repositories.AbstractRepository;
 
 @Repository
@@ -16,7 +15,7 @@ public interface ManagerDashboardRepository extends AbstractRepository {
 	@Query("SELECT COUNT(m) + 1 FROM Manager m WHERE m.yearsExperience > (SELECT m2.yearsExperience FROM Manager m2 WHERE m2.id = :managerId)")
 	Integer findManagerRanking(@Param("managerId") int managerId);
 
-	@Query("SELECT 65 - YEAR(CURRENT_DATE) + YEAR(m.dateOfBirth) FROM Manager m WHERE m.id = :managerId")
+	@Query("SELECT 65 - YEAR(CURRENT_DATE) + YEAR(m.dateBirth) FROM Manager m WHERE m.id = :managerId")
 	Integer findYearsToRetire(@Param("managerId") int managerId);
 
 	@Query("SELECT SUM(CASE WHEN l.status = 'ON_TIME' THEN 1 ELSE 0 END) AS onTimeLegs, SUM(CASE WHEN l.status = 'DELAYED' THEN 1 ELSE 0 END) AS delayedLegs FROM Leg l WHERE l.flight.manager.id = :managerId")
@@ -31,16 +30,16 @@ public interface ManagerDashboardRepository extends AbstractRepository {
 	@Query("SELECT l.status, COUNT(l) FROM Leg l WHERE l.flight.manager.id = :managerId GROUP BY l.status")
 	List<Object[]> findLegsGroupedByStatus(@Param("managerId") int managerId);
 
-	@Query("SELECT AVG(f.cost) FROM Flight f WHERE f.manager.id = :managerId")
+	@Query("SELECT AVG(f.cost.amount) FROM Flight f WHERE f.manager.id = :managerId")
 	Double findAverageFlightCost(@Param("managerId") int managerId);
 
-	@Query("SELECT MIN(f.cost) FROM Flight f WHERE f.manager.id = :managerId")
-	Money findMinimumFlightCost(@Param("managerId") int managerId);
+	@Query("SELECT MIN(f.cost.amount) FROM Flight f WHERE f.manager.id = :managerId")
+	Double findMinimumFlightCost(@Param("managerId") int managerId);
 
-	@Query("SELECT MAX(f.cost) FROM Flight f WHERE f.manager.id = :managerId")
-	Money findMaximumFlightCost(@Param("managerId") int managerId);
+	@Query("SELECT MAX(f.cost.amount) FROM Flight f WHERE f.manager.id = :managerId")
+	Double findMaximumFlightCost(@Param("managerId") int managerId);
 
-	@Query("SELECT STDDEV(f.cost) FROM Flight f WHERE f.manager.id = :managerId")
+	@Query("SELECT STDDEV(f.cost.amount) FROM Flight f WHERE f.manager.id = :managerId")
 	Double findStandardDeviationOfFlightCost(@Param("managerId") int managerId);
 
 }
