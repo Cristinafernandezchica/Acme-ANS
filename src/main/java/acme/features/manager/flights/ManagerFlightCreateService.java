0525f5875c4510bcff047d4432dec8ee0498f9ea
CRISTINA FERNANDEZ <correoparacristina100@gmail.com>
2025-03-27 02:50:22 +0100

    feat: Create a flight

---
+++ src/main/java/acme/features/manager/flights/ManagerFlightCreateService.java
@@ -0,0 +1,68 @@
+
+package acme.features.manager.flights;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import acme.client.components.models.Dataset;
+import acme.client.services.AbstractGuiService;
+import acme.client.services.GuiService;
+import acme.entities.flights.Flight;
+import acme.realms.Manager;
+
+@GuiService
+public class ManagerFlightCreateService extends AbstractGuiService<Manager, Flight> {
+
+	@Autowired
+	private ManagerFlightRepository repository;
+
+
+	@Override
+	public void authorise() {
+		super.getResponse().setAuthorised(true);
+	}
+
+	@Override
+	public void load() {
+		Flight flight;
+		Manager manager;
+
+		manager = (Manager) super.getRequest().getPrincipal().getActiveRealm();
+
+		flight = new Flight();
+		flight.setDraftMode(true);
+		flight.setManager(manager);
+		flight.setAirline(manager.getAirline());
+
+		super.getBuffer().addData(flight);
+	}
+
+	@Override
+	public void bind(final Flight flight) {
+		super.bindObject(flight, "tag", "indication", "cost", "description");
+	}
+
+	@Override
+	public void validate(final Flight flight) {
+		;
+	}
+
+	@Override
+	public void perform(final Flight flight) {
+		this.repository.save(flight);
+	}
+
+	@Override
+	public void unbind(final Flight flight) {
+		Dataset dataset;
+
+		dataset = super.unbindObject(flight, "tag", "indication", "cost", "description", "draftMode");
+		// dataset.put("scheduledDeparture", flight.getScheduledDeparture());
+		// dataset.put("scheduledArrival", flight.getScheduledArrival());
+		// dataset.put("originCity", flight.originCity());
+		// dataset.put("destinationCity", flight.destinationCity());
+		// dataset.put("layovers", flight.layovers());
+
+		super.getResponse().addData(dataset);
+	}
+
+}
