9f348bd7e9a921ce5210b6899750d5fcdc15e7a1
AngelAmoUS <angamosan@alum.us.es>
2025-03-29 14:38:29 +0100

    feat: add Booking List for Customers

---
+++ src/main/java/acme/features/authenticated/customer/booking/CustomerBookingListService.java
@@ -0,0 +1,54 @@
+
+package acme.features.authenticated.customer.booking;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import acme.client.components.models.Dataset;
+import acme.client.services.AbstractGuiService;
+import acme.client.services.GuiService;
+import acme.entities.booking.Booking;
+import acme.realms.Customer;
+
+@GuiService
+public class CustomerBookingListService extends AbstractGuiService<Customer, Booking> {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	private CustomerBookingRepository repository;
+
+	// AbstractGuiService interface -------------------------------------------
+
+
+	@Override
+	public void authorise() {
+		super.getResponse().setAuthorised(true);
+	}
+
+	@Override
+	public void load() {
+		Collection<Booking> bookings;
+		int customerId;
+
+		customerId = super.getRequest().getPrincipal().getActiveRealm().getId();
+		bookings = this.repository.findBookingsByCustomerId(customerId);
+
+		super.getBuffer().addData(bookings);
+	}
+
+	@Override
+	public void unbind(final Booking booking) {
+		Dataset dataset;
+
+		dataset = super.unbindObject(booking, "locatorCode", "purchaseMoment", "travelClass", "price");
+		if (booking.getFlight() != null)
+			dataset.put("flight", booking.getFlight().getTag());
+		else
+			dataset.put("flight", "Sin vuelo asociado");
+
+		super.getResponse().addData(dataset);
+	}
+
+}
