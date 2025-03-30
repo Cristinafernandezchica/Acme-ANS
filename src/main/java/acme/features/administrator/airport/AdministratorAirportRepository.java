b248658aef660c106dc706ba1cdff64a30fb91db
MARTA AGUILAR <marta.aguilar.morcillo1@gmail.com>
2025-03-30 20:20:34 +0200

    feat: Implement operations of administrator in airports

---
+++ src/main/java/acme/features/administrator/airport/AdministratorAirportRepository.java
@@ -0,0 +1,24 @@
+
+package acme.features.administrator.airport;
+
+import java.util.Collection;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import acme.client.repositories.AbstractRepository;
+import acme.entities.airports.Airport;
+
+@Repository
+public interface AdministratorAirportRepository extends AbstractRepository {
+
+	@Query("select a from Airport a where a.id = :id")
+	Airport findAirportById(int id);
+
+	@Query("select a from Airport a")
+	Collection<Airport> findAllAirports();
+
+	@Query("SELECT (COUNT(a) + COUNT(al)) FROM Airport a, Airline al WHERE a.iataCode = :iataCode OR al.iataCode = :iataCode")
+	long countByIataCode(String iataCode);
+
+}
---
+++ src/main/java/acme/features/administrator/airport/AdministratorAirportUpdateService.java
@@ -0,0 +1,79 @@
+
+package acme.features.administrator.airport;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import acme.client.components.models.Dataset;
+import acme.client.components.principals.Administrator;
+import acme.client.components.views.SelectChoices;
+import acme.client.services.AbstractGuiService;
+import acme.client.services.GuiService;
+import acme.entities.airports.Airport;
+import acme.entities.airports.OperationalScopeType;
+
+@GuiService
+public class AdministratorAirportUpdateService extends AbstractGuiService<Administrator, Airport> {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	private AdministratorAirportRepository repository;
+
+	// AbstractService<Employer, Job> -------------------------------------
+
+
+	@Override
+	public void authorise() {
+		super.getResponse().setAuthorised(true);
+	}
+
+	@Override
+	public void load() {
+		Airport airport;
+		int id;
+
+		id = super.getRequest().getData("id", int.class);
+		airport = this.repository.findAirportById(id);
+
+		super.getBuffer().addData(airport);
+	}
+
+	@Override
+	public void bind(final Airport airport) {
+		super.bindObject(airport, "name", "iataCode", "city", "country", "website", "email", "phoneNumber");
+	}
+
+	@Override
+	public void validate(final Airport airport) {
+		boolean confirmation;
+		String iataCodeValue;
+		boolean imposibleIataCode;
+
+		confirmation = super.getRequest().getData("confirmation", boolean.class);
+		super.state(confirmation, "confirmation", "acme.validation.confirmation.message");
+
+		iataCodeValue = super.getRequest().getData("iataCode", String.class);
+		imposibleIataCode = this.repository.countByIataCode(iataCodeValue) <= 0;
+		super.state(imposibleIataCode, "iataCode", "acme.validation.airport.iataCode.message");
+	}
+
+	@Override
+	public void perform(final Airport airport) {
+		this.repository.save(airport);
+	}
+
+	@Override
+	public void unbind(final Airport airport) {
+		SelectChoices choices;
+		Dataset dataset;
+
+		choices = SelectChoices.from(OperationalScopeType.class, airport.getOperationalScope());
+
+		dataset = super.unbindObject(airport, "name", "iataCode", "operationalScope", "city", "country", "website", "email", "phoneNumber");
+		dataset.put("confirmation", false);
+		dataset.put("operationalScopes", choices);
+
+		super.getResponse().addData(dataset);
+	}
+
+}
