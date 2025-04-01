95746849af9b5293c88a646543db80530e2c4f09
AngelAmoUS <angamosan@alum.us.es>
2025-04-01 16:24:26 +0200

    feat: add all files needed for operations at passengers as customer

---
+++ src/main/java/acme/features/customer/passenger/CustomerPassengerListService.java
@@ -0,0 +1,56 @@
+
+package acme.features.customer.passenger;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import acme.client.components.models.Dataset;
+import acme.client.services.AbstractGuiService;
+import acme.client.services.GuiService;
+import acme.entities.passenger.Passenger;
+import acme.realms.Customer;
+
+@GuiService
+public class CustomerPassengerListService extends AbstractGuiService<Customer, Passenger> {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	private CustomerPassengerRepository repository;
+
+	// AbstractGuiService interface -------------------------------------------
+
+
+	@Override
+	public void authorise() {
+		super.getResponse().setAuthorised(true);
+	}
+
+	@Override
+	public void load() {
+		Collection<Passenger> passengers;
+		Customer customer;
+		int bookingId;
+
+		customer = (Customer) super.getRequest().getPrincipal().getActiveRealm();
+
+		if (super.getRequest().getData().isEmpty())
+			passengers = this.repository.findPassengersByCustomerId(customer.getId());
+		else {
+			bookingId = super.getRequest().getData("bookingId", int.class);
+			passengers = this.repository.findPassengersByBookingId(bookingId);
+		}
+		super.getBuffer().addData(passengers);
+	}
+
+	@Override
+	public void unbind(final Passenger passenger) {
+		Dataset dataset;
+
+		dataset = super.unbindObject(passenger, "fullName", "email", "passportNumber", "dateOfBirth", "specialNeeds");
+
+		super.getResponse().addData(dataset);
+	}
+
+}
