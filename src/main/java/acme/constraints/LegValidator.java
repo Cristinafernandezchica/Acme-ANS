b81d5d098c35d92adb6e4f68ad27adc704f7daa6
CRISTINA FERNANDEZ <correoparacristina100@gmail.com>
2025-04-15 00:17:17 +0200

    feat: Add again the manager identifier number validation

--- src/main/java/acme/constraints/LegValidator.java
+++ src/main/java/acme/constraints/LegValidator.java
@@ -42,45 +42,6 @@ public class LegValidator extends AbstractValidator<ValidLeg, Leg> {
 			if (existingLeg != null && !existingLeg.equals(leg))
 				super.state(context, false, "flightNumber", "acme.validation.leg.flightNumber.not.unique");
 
-			// Doesn't overlap with another leg of the same flight  --> Se ha pasado al sservicio de publicación de Legs
-			/*
-			 * List<Leg> flightsLegs = this.repository.findLegsByFlightId(leg.getFlight().getId());
-			 * for (Leg l : flightsLegs) {
-			 * if (leg.getId() == l.getId())
-			 * continue;
-			 * 
-			 * if (MomentHelper.isInRange(leg.getScheduledDeparture(), l.getScheduledDeparture(), l.getScheduledArrival())) {
-			 * result = false;
-			 * super.state(context, false, "scheduledDeparture/scheduledArrival", "{acme.validation.leg.overlap.message}");
-			 * }
-			 * }
-			 */
-
-			// scheduledArrival >= scheduledDeparture + delta (5 min) --> Añadida al crear o actualizar una leg
-			/*
-			 * Date departureWithDelta = MomentHelper.deltaFromMoment(leg.getScheduledDeparture(), 5, ChronoUnit.MINUTES);
-			 * if (MomentHelper.isBefore(leg.getScheduledArrival(), departureWithDelta)) {
-			 * result = false;
-			 * super.state(context, false, "scheduledDeparture/scheduledArrival", "{acme.validation.leg.departure.arrival.difference.message}");
-			 * 
-			 * }
-			 */
-
-			// scheduledDeparture after scheduledArrival --> Se ha puesto al publicar una leg, al crearla y al actualizarla
-			/*
-			 * if (MomentHelper.isBefore(leg.getScheduledArrival(), leg.getScheduledDeparture())) {
-			 * result = false;
-			 * super.state(context, false, "scheduledDeparture", "{acme.validation.leg.departure.after.arrival.message}");
-			 * }
-			 */
-			// aircraft operative --> se ha puesto al publicar
-			/*
-			 * if (leg.getAircraft().getStatus().equals(Status.UNDER_MAINTENANCE)) {
-			 * result = false;
-			 * super.state(context, false, "aircraft", "{acme.validation.leg.operative.aircraft.message}");
-			 * }
-			 */
-
 		}
 		return !super.hasErrors(context);
 	}
