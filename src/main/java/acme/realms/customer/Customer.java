c6b81ebe5f11187b3b4a2206591eb754a844ca7d
AngelAmoUS <angamosan@alum.us.es>
2025-04-30 14:12:47 +0200

    refactor: move CustomerIdentifier method into create service

--- src/main/java/acme/realms/customer/Customer.java
+++ src/main/java/acme/realms/customer/Customer.java
@@ -1,22 +1,15 @@
 
 package acme.realms.customer;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.Transient;
 
 import acme.client.components.basis.AbstractRole;
 import acme.client.components.mappings.Automapped;
-import acme.client.components.principals.DefaultUserIdentity;
 import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.Optional;
 import acme.client.components.validation.ValidNumber;
 import acme.client.components.validation.ValidString;
-import acme.client.helpers.SpringHelper;
 import acme.constraints.ValidCustomer;
 import acme.constraints.ValidPhoneNumber;
 import lombok.Getter;
@@ -60,36 +53,4 @@ public class Customer extends AbstractRole {
 	@Automapped
 	private Integer				earnedPoints;
 
-
-	@Transient
-	public String getCustomerIdentifier() {
-		CustomerRepository repository;
-		repository = SpringHelper.getBean(CustomerRepository.class);
-		DefaultUserIdentity identity = this.getIdentity();
-
-		if (identity == null || identity.getName() == null || identity.getSurname() == null)
-			return null;
-
-		String name = identity.getName().trim().toUpperCase();
-		String surname = identity.getSurname().trim().toUpperCase();
-
-		if (name.isEmpty() || surname.isEmpty())
-			return null;
-
-		String initials = "" + name.charAt(0) + surname.charAt(0);
-
-		List<String> existingIdentifiers = repository.findAllIdentifiersStartingWith(initials);
-		existingIdentifiers.remove(this.identifier);
-
-		Set<String> existingSet = new HashSet<>(existingIdentifiers);
-
-		for (int i = 0; i <= 999999; i++) {
-			String numberPart = String.format("%06d", i);
-			String candidate = initials + numberPart;
-
-			if (!existingSet.contains(candidate))
-				return candidate;
-		}
-		return null;
-	}
 }
