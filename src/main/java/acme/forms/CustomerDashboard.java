c461c76bc00e28c3a778c8c55309481f96ea2511
AngelAmoUS <angamosan@alum.us.es>
2025-05-04 18:10:25 +0200

    fix: update attributes queries from customer dashboard

--- src/main/java/acme/forms/CustomerDashboard.java
+++ src/main/java/acme/forms/CustomerDashboard.java
@@ -1,11 +1,10 @@
 
-package acme.forms.customerDashboard;
+package acme.forms;
 
 import java.util.List;
 import java.util.Map;
 
 import acme.client.components.basis.AbstractForm;
-import acme.client.components.datatypes.Money;
 import acme.entities.booking.TravelClass;
 import lombok.Getter;
 import lombok.Setter;
@@ -17,14 +16,14 @@ public class CustomerDashboard extends AbstractForm {
 	private static final long			serialVersionUID	= 1L;
 
 	private List<String>				lastFiveDestinations;
-	private Money						moneySpentLastYear;
+	private Map<String, Double>			moneySpentLastYear;
 	private Map<TravelClass, Integer>	bookingCountByTravelClass;
 
 	private Integer						bookingCountLastFiveYears;
-	private Money						bookingAverageCostLastFiveYears;
-	private Money						bookingMinCostLastFiveYears;
-	private Money						bookingMaxCostLastFiveYears;
-	private Money						bookingStdDevCostLastFiveYears;
+	private Map<String, Double>			bookingAverageCostLastFiveYears;
+	private Map<String, Double>			bookingMinCostLastFiveYears;
+	private Map<String, Double>			bookingMaxCostLastFiveYears;
+	private Map<String, Double>			bookingStdDevCostLastFiveYears;
 
 	private Integer						passengerCount;
 	private Double						passengerAverage;	//Cuando haga la media, el double lo tengo que redondear
